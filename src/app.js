f6682adc126eca9e8686597726682103b2637a88
Jayraj-parki <jayrajfd@gmail.com>
2022-03-02 01:46:19 +0530

    added static path

--- src/app.js
+++ src/app.js
@@ -36,6 +36,7 @@ app.use("/api/review",reviewRoute)
 app.use("/api/allData",allRoute)
 app.use("/api/issues",issueRoute)
 
+app.use(express.static(path.join(__dirname,"../frontend/build")));
 
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname,"../frontend/build", "index.html"));
