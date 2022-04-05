175435ed9d15678cc635806df125ced03ebddfa3
Jayraj-parki <jayrajfd@gmail.com>
2022-04-05 15:31:09 +0530

    updated sidebar and navbar profile icon

--- frontend/src/components/avatar/Avatar.js
+++ frontend/src/components/avatar/Avatar.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import style from "./avatar.module.scss"
+import { useSelector } from "react-redux"
 export default function Avatar() {
+    const user = useSelector((state) => state.changeTheUserLog)
+    
     return (
         <NavLink to="/profile" className={style.avatar + " col-auto text-decoration-none d-none d-lg-flex  p-0 "}>
-            <img src="/images/1.png" alt="" />
+            <img src={user.profile || "/images/1.png"} alt="" />
         </NavLink>
     )
 }
